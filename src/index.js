4d704bf89cecad74adb715562708d3110f31953f
Brayan-724 <bjqf0724@gmail.com>
2021-06-08 19:14:14 -0500

    Auto commited

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 // Modules
 const express = require('express');
 const { join } = require("path");
+const morgan = require('morgan');
+const cookieParser = require("cookie-parser");
 
 // DotEnv
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config({ path: ".env" });
 
 // Setup
 const app = express();
 
-// Config
+// Config ejs engine
 app.set('views', join(__dirname, 'views'));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+
+// Config
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(join(__dirname, "public")));
 
 
 
-
 app.use((req, res) => {
-	res.render("Test")
+	res.render("Tests")
 })
 
 module.exports = app;
