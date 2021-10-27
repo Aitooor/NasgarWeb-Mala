e7fd1e3f1789444df96b6434528e9d43a91a4691
Brayan-724 <bjqf0724@gmail.com>
2021-10-26 21:29:58 -0700

    Add Card menu prefab and use it on staff menu

--- src/routes/client/index.js
+++ src/routes/client/index.js
@@ -37,7 +37,48 @@ module.exports = require("../../lib/Routes/exports")("/", (router, waRedirect, d
   router.get("/bans",     (_req, res) => { res.status(501).render("prefabs/soon") });
   router.get("/settings", (_req, res) => { res.status(501).render("prefabs/soon") });
   router.get("/profile",  (_req, res) => { res.status(501).render("prefabs/soon") });
-  router.get("/staff",    (_req, res) => { res.status(501).render("prefabs/soon") });
+  router.get("/staff/shop",    (_req, res) => { 
+    res.render("prefabs/cards_menu", {
+      title: "Shop Menu",
+      data: [
+        {
+          url: "/staff/shop/panel",
+          title: "Products",
+          icon: "inventory_2/v9"
+        },
+        {
+          url: "/staff/shop/order",
+          title: "Order (Not implemented)",
+          icon: "reorder/v15"
+        }
+      ]
+    }) 
+  });
+
+  router.get("/staff/shop/order",  (_req, res) => { res.status(501).render("prefabs/soon") });
+    
+  router.get("/staff",    (_req, res) => { 
+    res.render("prefabs/cards_menu", {
+      title: "Staff Menu",
+      data: [
+        {
+          url: "/staff/staff-timings",
+          title: "Timings",
+          icon: "schedule/v16"
+        },
+        {
+          url: "/staff/shop",
+          title: "Shop",
+          icon: "store/v11"
+        },
+        {
+          url: "#",
+          title: "Test_Empty",
+          icon: "block/v15"
+        }
+      ]
+    }) 
+  });
     
   router.get("/", async (req, res) => {
     if(Date.now() - lastRequestTime >= 1000) {
